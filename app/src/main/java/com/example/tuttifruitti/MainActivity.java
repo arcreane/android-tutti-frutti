a9bbadc4ac9a17f13049f0d8aecd44b620a65282
Brahim Rabeb <rabeb.brahim@edu.itescia.fr>
2021-03-03 23:22:23 +0100

    new modifications

--- app/src/main/java/com/example/tuttifruitti/MainActivity.java
+++ app/src/main/java/com/example/tuttifruitti/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.tuttifruitti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
+    }
+
+    public void getEasy(View view) {
     }
 }

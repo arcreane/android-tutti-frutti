5d16a42a207b2c8a4dc79d2e9d64b4a9eceec19d
Samia Ouerfelli <73239375+souerfelli@users.noreply.github.com>
2021-03-04 12:00:40 +0100

    class difficulty, onclik

--- app/src/main/java/com/example/tuttifruitti/Difficulty.java
+++ app/src/main/java/com/example/tuttifruitti/Difficulty.java
@@ -1,8 +1,41 @@
 package com.example.tuttifruitti;
 
-public class Difficulty {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Difficulty extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Button playButton = findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_game);
+
+            }
+        });
+
+
+
+
+
+
+    }
 
 
 
 
 }
+
+
+
+

214ff9e40cd8352577c23a7580f68f067c93e54f
Brahim Rabeb <rabeb.brahim@edu.itescia.fr>
2021-03-04 10:00:19 +0100

    lancement du jeu

--- app/src/main/java/com/example/tuttifruitti/MainActivity.java
+++ app/src/main/java/com/example/tuttifruitti/MainActivity.java
@@ -4,15 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_main);
     }
 
-    public void getEasy(View view) {
+    final Button startButton= findViewById(R.id.bt_start);
+
+    public void startGame(View view) {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_difficulty);
+            }
+        });
+
+    }
+
+    public void showScores(View view) {
+    }
+
+    public void quitGame(View view) {
     }
 }
